203f0c338a0052547758560e3b7ae5aab9b67568
Yatharth <yathartharora1999@gmail.com>
2021-02-12 23:49:48 +0530

    Book List Updated

--- Ethereum/Contract/Blog.sol
+++ Ethereum/Contract/Blog.sol
@@ -10,11 +10,18 @@ contract Blog{
         string pdf;
     }
     
+    struct Books{
+        string Book_name;
+        string author;
+        string summary;
+    }
+    
     constructor() public {
         manager = msg.sender;
     }
     
     File[] public f;
+    Books[] public book;
     
     modifier restricted{
         require(msg.sender==manager);
@@ -26,7 +33,15 @@ contract Blog{
         f.push(File(name,pdf));
     }
     
+    function BookUpload(string bname, string auth, string sum) public restricted{
+        book.push(Books(bname,auth,sum));
+    }
+    
     function display() public view returns(File[]){
         return f;
+    }
+    
+    function Bookdisplay() public view returns(Books[]){
+        return book;
     }
 }
--- Ethereum/blog.js
+++ Ethereum/blog.js
@@ -1,7 +1,7 @@
 import web3 from './web3';
 import Mail from './build/Blog.json';
 
-const instance = new web3.eth.Contract(JSON.parse(Mail.interface),'0xd5AE95A8cA038223a9Fc491b44C721917fF92587');
+const instance = new web3.eth.Contract(JSON.parse(Mail.interface),'0x34A4355e91c64144901d02C299A76201130fc6d5');
 
 
 export default instance;
